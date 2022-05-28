7a84964c3fca9084c505a7fd4e0133f16a6fffe5
bola8dev <106438472+bola8dev@users.noreply.github.com>
2022-05-28 20:46:33 +0200

    Add files via upload

---
+++ const miLista = {.js
@@ -0,0 +1,13 @@
+const miLista = {
+    nombre : "paco",
+    edad : 37,
+    eres_Desarrollador : "estoy en ello",
+    fecha_De_Nacimiento : Date("february 21 1985"),
+    libro_Favorito : {
+        titulo : "los pilares de la tierra",
+        autor : "ken follet",
+        fecha : 1989,
+        URL : "https://books.google.es/books/about/Los_pilares_de_la_Tierra_Saga_Los_pilare.html?id=mKoInmOU130C&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false",
+
+    }
+}
